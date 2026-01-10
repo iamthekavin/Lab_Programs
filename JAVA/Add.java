6cbce0a5238c49fa3af9b6feef7a4b020a8082a2
Kavin <dev.iamkavin@gmail.com>
2026-01-10 22:04:56 +0530

    Update Add.java

--- JAVA/Add.java
+++ JAVA/Add.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class Add {
     public static void main(String a[]){
-        int num1 = 10;
-        int num2 = 20;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Second number: ");
+        int num2 = sc.nextInt();
         int Result = num1 + num2;
-        System.out.println(Result);
+        System.out.println("Sum = " + Result);
     }
 }
+
