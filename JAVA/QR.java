2b47ce614329eb52a5a1ae308f63da928aa26dab
Kavin <dev.iamkavin@gmail.com>
2026-01-10 22:32:20 +0530

    Update QR.java

--- JAVA/QR.java
+++ JAVA/QR.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 class QR {
     public static void main(String[] args) {
-        int dividend = 100;
-        int divisor = 2;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter dividend: ");
+        int dividend =sc.nextInt();
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
 
         System.out.println("Quotient = " + (dividend / divisor));
         System.out.println("Remainder = " + (dividend % divisor));
+        sc.close();
     }
 }
+
