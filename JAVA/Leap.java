959124e1dad5dd2d81d1fb5e5e4246afa520108c
Kavin <dev.iamkavin@gmail.com>
2026-01-10 22:25:36 +0530

    Update Leap.java
    Updated with Scanner class

--- JAVA/Leap.java
+++ JAVA/Leap.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
 public class Leap {
     public static void main(String a[]){
-        int year = 2018;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Year: ");
+        int year = sc.nextInt();
         if(year % 4 == 0){
-            System.out.println("is a leap year");
+            System.out.println(year + " is a leap year");
         }
         else{
-            System.out.println("is not a leap year");
+            System.out.println(year + " is not a leap year");
         }
+        sc.close();
     }
 }
